d5ea437121b1ef629586af07b19e3fc20ad62e38
azhar759 <azhar759@users.noreply.github.com>
2016-08-08 23:18:33 +0400

    Fix indentation

--- src/SimulatedAnnealing/Solution.java
+++ src/SimulatedAnnealing/Solution.java
@@ -1,25 +1,25 @@
 package SimulatedAnnealing;
 
 public class Solution {
-	public int distance;
-	public int[] paths;
+  public int distance;
+  public int[] paths;
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder("Distance = " + distance
-				+ "; Path = ");
-		for (int i = 0; i < paths.length - 1; i++) {
-			sb.append(paths[i] + " - ");
-		}
-		sb.append(paths[paths.length - 1]);
-		return sb.toString();
-	}
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("Distance = " + distance
+        + "; Path = ");
+    for (int i = 0; i < paths.length - 1; i++) {
+      sb.append(paths[i] + " - ");
+    }
+    sb.append(paths[paths.length - 1]);
+    return sb.toString();
+  }
 
-	public Solution() {
-	}
+  public Solution() {
+  }
 
-	public Solution(int distance, int[] paths) {
-		this.distance = distance;
-		this.paths = paths;
-	}
+  public Solution(int distance, int[] paths) {
+    this.distance = distance;
+    this.paths = paths;
+  }
 }
